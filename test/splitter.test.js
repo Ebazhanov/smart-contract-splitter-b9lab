5c3ad02d5934867bb63e5e8a2bb6dcc69f08d9f3
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-05-01 22:21:11 +0200

    updates regarding comments

--- test/splitter.test.js
+++ test/splitter.test.js
@@ -5,34 +5,31 @@ const Splitter = artifacts.require("Splitter");
 
 contract('Splitter', accounts => {
     // Setup accounts
-    const sender = accounts[1];
-    const Bob = accounts[2];
-    const Alice = accounts[3];
+    const [sender, Bob, Alice] = accounts;
 
     let splitterInstance;
 
     beforeEach(async () => {
-        splitterInstance = await Splitter.new();
+        splitterInstance = await Splitter.new({ from: sender });
     });
 
     describe('Splitting functionality', () => {
-
         it('Should split amount evenly', async () => {
             // Split sent ether between receivers.
             const amount = 10;
             const txObject = await splitterInstance.split(Bob, Alice, {from: sender, value: amount});
-            assert(txObject.receipt.status, 'Split failed');
+            assert.strictEqual(txObject.receipt.status, 1, 'Split failed');
 
             // Checking if everything as expected
             await checkFunds(Bob, amount / 2);
             await checkFunds(Alice, amount / 2);
+        });
 
-            it('Should split odd number of ether correctly', async () => {
-                // Split sent ether between receivers.
-                const amount = 11;
-                const txObject = await splitterInstance.split(Bob, Alice, {from: sender, value: amount});
-                assert(txObject.receipt.status, 'Split failed');
-            });
+        it('Should split odd number of ether correctly', async () => {
+            // Split sent ether between receivers.
+            const amount = 11;
+            const txObject = await splitterInstance.split(Bob, Alice, {from: sender, value: amount});
+            assert(txObject.receipt.status, 'Split failed');
         });
 
     });
