2020b58351d40bdb9881ae39149e4d6dce72b029
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-05-09 10:31:45 +0200

    Module 7 - "Webpack Build for Web"

---
+++ app-web/Readme.md
@@ -0,0 +1,5 @@
+# Module 6 - Webpack build for Web
+----------------------------------
+
+* `$ .node_modules/.bin/truffle unbox webpack`
+* `$ php -S 0.0.0.0:8000 -t ./build/app`
---
+++ app-web/truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*", // Match any network id
+            gas: 500000 // Or any higher value that you deem necessary
+        }
+    }
+};
